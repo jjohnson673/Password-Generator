816edc9a16016f2231bc56d650254dd058a99492
jjohnson673 <jkjohnson673@gmail.com>
2022-09-30 14:13:34 -0400

    added in an additional symbol

--- Assets/script.js
+++ Assets/script.js
@@ -54,7 +54,7 @@ var userWantsSymbols = window.confirm("Would you like to include symbols in your
 var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numbersList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
+var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "+"]
 
 /*add in a 'cart' to hold the TRUE items */
 var variationsCart = []
